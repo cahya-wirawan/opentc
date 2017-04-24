1c0fcaf5c7dc07c048c33c8220ebf98c119a5b7a
Cahya Wirawan <cahya.wirawan@gmail.com>
2017-04-24 12:20:06 +0200

    Moved setup_config, setup_logging to an external package (opentc-util)

--- opentc/__init__.py
+++ opentc/__init__.py
@@ -2,71 +2,7 @@
 OpenTC is text classification engine running as client server architecture. It listen on port 3333
 
 """
-import os
-import logging.config
-import yaml
-import logging
 from .config import __version__
 
 
 __author__ = "Cahya Wirawan <Cahya.Wirawan@gmail.com>"
-
-
-def setup_logging(
-        config_directories=None,
-        config_file=None,
-        default_level=logging.INFO
-):
-    """Setup logging configuration
-
-    """
-    config_found = False
-    config_file_path = None
-    if config_file:
-        config_file_path = config_file
-        if os.path.isfile(config_file_path) and os.access(config_file_path, os.R_OK):
-            config_found = True
-    else:
-        for directory in config_directories:
-            if directory is None:
-                continue
-            config_file_path = os.path.join(directory, "logging.yml")
-            if os.path.isfile(config_file_path) and os.access(config_file_path, os.R_OK):
-                config_found = True
-                break
-    if config_found:
-        with open(config_file_path, 'rt') as ymlfile:
-            config = yaml.safe_load(ymlfile.read())
-        logging.config.dictConfig(config)
-    else:
-        logging.basicConfig(level=default_level)
-
-
-def setup_config(
-        config_directories=None,
-        config_file=None
-):
-    """Setup configuration
-
-    """
-    config_found = False
-    config_file_path = None
-    if config_file:
-        config_file_path = config_file
-        if os.path.isfile(config_file_path) and os.access(config_file_path, os.R_OK):
-            config_found = True
-    else:
-        for directory in config_directories:
-            if directory is None:
-                continue
-            config_file_path = os.path.join(directory, "opentc.yml")
-            if os.path.isfile(config_file_path) and os.access(config_file_path, os.R_OK):
-                config_found = True
-                break
-    if config_found:
-        with open(config_file_path, 'rt') as ymlfile:
-            config = yaml.safe_load(ymlfile.read())
-        return config
-    else:
-        print("The configuration file is not found.")
-        exit(1)
